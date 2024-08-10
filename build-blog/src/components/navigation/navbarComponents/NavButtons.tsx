8beaa53d468eaae4a1401b3fd67c1b9435b00109
Peter <Peterlee2094@gmail.com>
2024-08-10 17:22:40 -0500

    add theme local storage

--- build-blog/src/components/navigation/navbarComponents/NavButtons.tsx
+++ build-blog/src/components/navigation/navbarComponents/NavButtons.tsx
@@ -3,26 +3,36 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 
 export default function NavThemeButton() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState<boolean | null>(null);
+
   useEffect(() => {
-    if (darkMode) {
-      document.body.classList.remove("light");
-      document.body.classList.add("dark");
-    } else if (!darkMode) {
-      document.body.classList.remove("dark");
-      document.body.classList.add("light");
-    } else if (window.matchMedia("prefers-color-scheme:dark)")) {
-      document.body.classList.remove("light");
-      document.body.classList.add("dark");
+    const theme = localStorage.getItem("theme");
+    if (theme) {
+      setDarkMode(theme === "dark");
+    } else if (window.matchMedia("(prefers-color-scheme:dark)")) {
+      setDarkMode(true);
     } else {
-      document.body.classList.remove("dark");
-      document.body.classList.add("light");
+      setDarkMode(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode !== null) {
+      if (darkMode) {
+        localStorage.setItem("theme", "dark");
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+      } else {
+        localStorage.setItem("theme", "light");
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+      }
     }
   }, [darkMode]);
 
   return (
     <div
-      className="shadow-[0px_1px_2px_0px_rgba(60,64,67,0.3),0px_1px_3px_1px_rgba(60,64,67,0.15)] rounded-full bg-navbar-cta flex items-center justify-center h-[32px] w-[32px] hover:cursor-pointer select-none"
+      className="nav-theme-btn shadow-[0px_1px_2px_0px_rgba(60,64,67,0.3),0px_1px_3px_1px_rgba(60,64,67,0.15)] rounded-full bg-navbar-cta flex items-center justify-center h-[32px] w-[32px] hover:cursor-pointer select-none"
       onClick={() => {
         setDarkMode(!darkMode);
       }}
