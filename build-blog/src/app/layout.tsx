6bcc3d6895bad0efae389f635a28b7c3acbd139a
Peter <Peterlee2094@gmail.com>
2024-08-13 10:27:31 -0500

    add modal portal

--- build-blog/src/app/layout.tsx
+++ build-blog/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={"light bg-background"}>
+        <div id={"__modal"}></div>
         <Navbar />
         {children}
       </body>
