ff9af5ad63bc3ca294f02cf6f3416c9ef0ae7840
Peter <Peterlee2094@gmail.com>
2024-08-19 10:23:43 -0500

    create build page

--- build-blog/src/app/build/page.tsx
+++ build-blog/src/app/build/page.tsx
@@ -1,10 +1,8 @@
-import { FileInput } from "@/components/pageComponents/buildComponents/build";
-
 export default function Build() {
   return (
     <main className="">
       <article className="flex flex-col items-center justify-center w-[42rem] m-auto">
-        <FileInput />
+        {/* <FileInput post={post} /> */}
       </article>
     </main>
   );
