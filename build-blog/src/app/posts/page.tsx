a1537097fbf3b4487599ff4e13fbc485ba687186
Peter <Peterlee2094@gmail.com>
2024-08-19 10:24:34 -0500

    get all post cards

--- build-blog/src/app/posts/page.tsx
+++ build-blog/src/app/posts/page.tsx
@@ -2,13 +2,15 @@ import {
   PostsIntro,
   PostsSearch,
 } from "@/components/pageComponents/postsComponents/Posts";
+import { getPostCards } from "@/lib/api";
 
 export default function Posts() {
+  const allPostCards = getPostCards();
   return (
     <main>
       <article className="w-[42rem] flex flex-col items-center justify-center m-auto gap-10">
         <PostsIntro />
-        <PostsSearch />
+        <PostsSearch defaultPosts={allPostCards} />
       </article>
     </main>
   );
