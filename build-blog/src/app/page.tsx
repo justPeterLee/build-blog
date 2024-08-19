955967499962b38a61a5e303b0e809f9fc7834ab
Peter <Peterlee2094@gmail.com>
2024-08-19 10:26:14 -0500

    add get post cards

--- build-blog/src/app/page.tsx
+++ build-blog/src/app/page.tsx
@@ -3,14 +3,16 @@ import {
   Introduction,
   RecentProjects,
 } from "@/components/pageComponents/homeComponents/Home";
+import { getPostCards } from "@/lib/api";
 
 export default function Home() {
+  const recentPostCards = getPostCards();
   return (
     <main className="">
       <article className="w-[42rem] flex flex-col items-center justify-center m-auto gap-10">
         <Introduction />
         <CurrentProject />
-        <RecentProjects />
+        <RecentProjects postCards={recentPostCards} />
       </article>
     </main>
   );
