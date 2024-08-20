624f871ba94d6b21838d074b7dddea1dcebcacd0
Peter <Peterlee2094@gmail.com>
2024-08-20 05:36:11 -0500

    bug fixes

--- build-blog/src/app/posts/[postId]/page.tsx
+++ build-blog/src/app/posts/[postId]/page.tsx
@@ -3,12 +3,12 @@ import { getPostById } from "@/lib/api";
 import notFound from "./not-found";
 
 export default function Post({ params }: { params: { postId: string } }) {
-  console.log(params);
-  const post = getPostById(params.postId) as string;
+  const post = getPostById(params.postId) as string | null;
 
   if (post === null) {
     return notFound();
   }
+
   return (
     <main className="post flex justify-center">
       <PostById post={post ? post : ""} />
--- build-blog/src/components/pageComponents/postsComponents/Post.tsx
+++ build-blog/src/components/pageComponents/postsComponents/Post.tsx
@@ -5,17 +5,13 @@ import Markdown from "markdown-to-jsx";
 import { ReactNode } from "react";
 
 type MarkdownOverride = { children: ReactNode; props: any };
+
 const ArticleWrap = ({ children, ...props }: MarkdownOverride) => (
   <article {...props} className="w-[42rem]">
     {children}
   </article>
 );
 
-const myParagraph = ({ children, ...props }: MarkdownOverride) => (
-  <p {...props} className="bg-card p-4 rounded-lg text-secondary-text">
-    {children}
-  </p>
-);
 export default function PostById({ post }: { post: string }) {
   return (
     <Markdown
