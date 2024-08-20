a2689bf92acb8f96291dd21bfad9d0aaa10eee4c
Peter <Peterlee2094@gmail.com>
2024-08-20 05:59:58 -0500

    create jsx view port component

--- build-blog/src/components/build/ViewPort.tsx
+++ build-blog/src/components/build/ViewPort.tsx
@@ -1,6 +1,23 @@
 "use client";
+
+import { useState } from "react";
+
 export default function ViewPort() {
   return (
-    <article className="w-[42rem] p-4 border-2 border-solid-black"></article>
+    <article className="w-full h-full min-h-32 p-2 bg-cta-active rounded-xl shadow-inner">
+      <JsxViewPort />
+    </article>
+  );
+}
+
+function JsxViewPort() {
+  const [elementList, setElementList] = useState<JsxElementList[]>([]);
+
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      {elementList.length === 0 && (
+        <p className="text-secondary-text text-lg">add componets</p>
+      )}
+    </div>
   );
 }
