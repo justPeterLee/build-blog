8e184cc10e59af8c74661717bfbb356d5cbc8bd7
Peter <Peterlee2094@gmail.com>
2024-08-28 10:06:53 -0500

    remove elementOrigin

--- build-blog/src/components/build/buildContext/BuildContext.tsx
+++ build-blog/src/components/build/buildContext/BuildContext.tsx
@@ -1,80 +1,45 @@
 import {
-  fn,
   getViewPortHeight,
   setAnimationElement,
-  swapElementAnimation,
   swapOrder,
-  switchOrder,
 } from "@/lib/buildUtils/element-utils";
-import {
-  SpringRef,
-  SpringValue,
-  useSpring,
-  useSprings,
-} from "@react-spring/web";
+import { SpringRef, SpringValue, useSpring } from "@react-spring/web";
 import { createContext, ReactNode, useEffect, useRef, useState } from "react";
 
 export type ElementSpringType = SpringRef<SpringType>;
 
-export const BuildContext = createContext<
-  | {
-      addElement: () => void;
-      getElementList: (type: "origin" | "ref" | "state") => JsxElementList[];
-      viewPortSpring: {
-        scaleY: SpringValue<number>;
-      };
-      vpElementSprings: {
-        y: SpringValue<number>;
-        visibility: SpringValue<string>;
-      }[];
-      initialRender: () => void;
-      insertFunc: {
-        swapInsert: (newIndex: number) => {
-          elementList: JsxElementList[];
-          elementListOrder: number[];
-        };
-        hideInsert: () => void;
-        addSpringInstance: (id: string, spring: ElementSpringType) => void;
-        swapElement: (
-          id: string,
-          newIndex: number
-        ) => JsxElementList[] | undefined;
-      };
-    }
-  | undefined
->(undefined);
+interface BuildContextType {
+  addElement: () => void;
+  getElementList: (type: "ref" | "state") => JsxElementList[];
+  viewPortSpring: {
+    scaleY: SpringValue<number>;
+  };
+  initialRender: () => void;
+  insertFunc: {
+    hideInsert: () => void;
+    addSpringInstance: (id: string, spring: ElementSpringType) => void;
+    swapElement: (id: string, newIndex: number) => JsxElementList[] | undefined;
+  };
+}
+
+export const BuildContext = createContext<BuildContextType | undefined>(
+  undefined
+);
 
 export function BuildContextProvider({ children }: { children: ReactNode }) {
-  // do not perform any action on (only used to maintain state)
-  const _elementListOrigin = useRef<JsxElementList[]>([
+  const elementListRef = useRef<JsxElementList[]>([
     { id: "insert-here", component: "insert-here", content: "" },
-    { id: "unique-id", component: "Text", content: "" },
-    { id: "unique-id2", component: "Text", content: "" },
-    { id: "unique-id4", component: "Text", content: "" },
-    { id: "unique-id5", component: "Text", content: "" },
-    { id: "unique-id3", component: "Video", content: "" },
   ]);
 
-  const elementListRef = useRef<JsxElementList[]>(_elementListOrigin.current); //
-  const elementListOrder = useRef(
-    elementListRef.current.map((_, index) => index)
-  );
   const [elementListState, setElementListState] = useState<JsxElementList[]>([
     { id: "insert-here", component: "insert-here", content: "" },
-    { id: "unique-id", component: "Text", content: "" },
-    { id: "unique-id2", component: "Text", content: "" },
-    { id: "unique-id4", component: "Text", content: "" },
-    { id: "unique-id5", component: "Text", content: "" },
-    { id: "unique-id3", component: "Video", content: "" },
   ]);
 
   const elementSpringObj = useRef<{
     [key: string]: ElementSpringType;
   }>({});
-  const getElementList = (type: "origin" | "ref" | "state") => {
-    if (type === "origin") {
-      return _elementListOrigin.current;
-    } else if (type === "ref") {
+  const getElementList = (type: "ref" | "state") => {
+    if (type === "ref") {
       return elementListRef.current;
     } else if (type === "state") {
       return elementListState;
@@ -87,46 +52,14 @@ export function BuildContextProvider({ children }: { children: ReactNode }) {
     scaleY: 1,
   }));
 
-  const [vpElementSprings, vpElementSpringsApi] = useSprings(
-    _elementListOrigin.current.length,
-    () => {
-      return { y: 0, visibility: "hidden" };
-    }
-  );
-
   // mutation functions
   // swap insert
-  const swapInsert = (newIndex: number) => {
-    const newLists = switchOrder(
-      elementListRef.current,
-      elementListOrder.current,
-      elementListOrder.current.indexOf(0),
-      newIndex
-    );
-
-    elementListRef.current = newLists.elementList;
-    elementListOrder.current = newLists.order;
-
-    // console.log("test");
-    // vpElementSpringsApi.set(
-    //   fn(elementListRef.current, elementListOrder.current, true)
-    // );
-    // viewPortSpringApi.set({
-    //   scaleY: getViewPortHeight(elementListRef.current, true),
-    // });
-
-    return {
-      elementList: elementListRef.current,
-      elementListOrder: elementListOrder.current,
-    };
-  };
 
   const swapElement = (id: string, newIndex: number) => {
     const currentIndex = elementListRef.current.findIndex(
       (element) => element.id === id
     );
     if (currentIndex < 0) return elementListRef.current;
-    // if (currentIndex === newIndex) return elementListRef.current;
 
     elementListRef.current = swapOrder(
       elementListRef.current,
@@ -148,9 +81,6 @@ export function BuildContextProvider({ children }: { children: ReactNode }) {
 
   // hide insert
   const hideInsert = () => {
-    // vpElementSpringsApi.set(
-    //   fn(elementListRef.current, elementListOrder.current, false)
-    // );
     setAnimationElement(
       elementListRef.current,
       elementSpringObj.current,
@@ -171,42 +101,30 @@ export function BuildContextProvider({ children }: { children: ReactNode }) {
       { id: "insert-here", component: "insert-here", content: "" },
       { id: "unique-id4", component: "Video", content: "" },
     ]);
-    // _elementListOrigin.current = [
-    //   { id: "insert-here", component: "insert-here", content: "" },
-    //   { id: "unique-id", component: "Text", content: "" },
-    //   { id: "unique-id2", component: "Text", content: "" },
-    //   { id: "unique-id3", component: "Video", content: "" },
-    // ];
   };
 
   const addSpringInstance = (id: string, spring: ElementSpringType) => {
     elementSpringObj.current[id] = spring;
   };
+
   const initialRender = () => {
     setAnimationElement(elementListState, elementSpringObj.current, false);
   };
-
-  //   useEffect(() => {
-  //     vpElementSpringsApi.set(
-  //       fn(elementListRef.current, elementListOrder.current)
-  //     );
-  //   }, []);
 
   useEffect(() => {
     viewPortSpringApi.set({
       scaleY: getViewPortHeight(elementListRef.current, false),
     });
-    // viewPortSpringApi.set(getViewPortHeight(elementListRef.current,true));
   }, [elementListState]);
+
   return (
     <BuildContext.Provider
       value={{
         addElement,
         getElementList,
         viewPortSpring,
-        vpElementSprings,
         initialRender,
-        insertFunc: { swapInsert, hideInsert, addSpringInstance, swapElement },
+        insertFunc: { hideInsert, addSpringInstance, swapElement },
       }}
     >
       {children}
